d0c3f68a6e9b67be3f7e030604c3ccb10ea87425
Cleber <cleber@dxs.com.br>
2019-12-21 10:30:45 -0500

    dsdsd

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,18 +61,12 @@ const TemplateWrapper = ({ children }) => (
         </div>
       </nav>
 
-      <ul class="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-
       <div id="test1" class="col s12">Test 1</div>
       <div id="test2" class="col s12">Test 2</div>
       <div id="test3" class="col s12">Test 3</div>
       <div id="test4" class="col s12">Test 4</div>
 
-    
+
     <div className="container">
       <HelmetDatoCms
         favicon={data.datoCmsSite.faviconMetaTags}
