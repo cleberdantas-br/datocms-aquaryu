604cd857717a9d7edcb9cd0235abcdf1a8962bf2
Cleber <cleber>
2019-12-22 12:37:11 -0500

    obrigado

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,28 @@ import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-
-
-
+import { createMuiTheme } from '@material-ui/core/styles';
 import '../styles/index.sass'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#ff4400',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    // error: will use the default color
+  },
+});
+
+
 
 
 const TemplateWrapper = ({ children }) => (
