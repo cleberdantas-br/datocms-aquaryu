650cb44d525b9124721c1f418786fceae84650f7
Cleber <cleber>
2019-12-21 21:34:59 -0500

    wow

--- src/components/layout.js
+++ src/components/layout.js
@@ -60,32 +60,6 @@ const TemplateWrapper = ({ children }) => (
   `}
   render={data => (
 
-    <nav class="nav-extended">
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Logo</a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
-          </ul>
-        </div>
-        <div class="nav-content">
-          <ul class="tabs tabs-transparent">
-            <li class="tab"><a href="#test1">Test 1</a></li>
-            <li class="tab"><a class="active" href="#test2">Test 2</a></li>
-            <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
-            <li class="tab"><a href="#test4">Test 4</a></li>
-          </ul>
-        </div>
-      </nav>
-
-      <div id="test1" class="col s12">Test 1</div>
-      <div id="test2" class="col s12">Test 2</div>
-      <div id="test3" class="col s12">Test 3</div>
-      <div id="test4" class="col s12">Test 4</div>
-
-
 
     <div className="container">
       <HelmetDatoCms
@@ -105,7 +79,7 @@ const TemplateWrapper = ({ children }) => (
           />
           <ul className="sidebar__menu">
             <li>
-            <Container maxWidth="sm">
+
                  <Box my={4}>
                    <Typography variant="h4" component="h1" gutterBottom>
                      Gatsby v4-beta example
@@ -116,7 +90,7 @@ const TemplateWrapper = ({ children }) => (
                    <ProTip />
                    <Copyright />
                  </Box>
-               </Container>
+              
               <Link to="/">Inicio vBeta</Link>
             </li>
             <li>
