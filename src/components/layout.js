a5b014e7e74d6e905339434fc95fc1547800cea1
Cleber <cleber>
2019-12-23 00:42:24 -0500

    Ok

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/core/MenuIcon'
+import MenuIcon from '@material-ui/icons/MenuIcon'
 
 
 import '../styles/index.sass'
